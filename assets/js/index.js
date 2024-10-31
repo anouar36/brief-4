bf4727fa8da8a63bb8e6d7c74b99464ae8fd19f0
anouar <anwar.class36flow@gmail.com>
2024-10-31 14:18:19 +0100

    version2 MODIFICATION HTML AND JS

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,19 +1,29 @@
-
-
 function showSection(sectionNumber) {
-    
-  document
-    .querySelectorAll(".section")
-    .forEach((section) => section.classList.add("d-none"));
+  document.querySelectorAll(".section").forEach((section) => section.classList.add("d-none"));
   document.getElementById("section" + sectionNumber).classList.remove("d-none");
 }
 
 function nextSection(sectionNumber) {
-    const valueInput = document.getElementById("exampleInputName").value;
-    console.log(valueInput);
   showSection(sectionNumber);
 }
 
 function previousSection(sectionNumber) {
   showSection(sectionNumber);
 }
+
+function submiform(event) {
+  event.preventDefault();
+  const firstName = document.getElementById("InputName").value;
+  const lastname = document.getElementById("InputLastName").value;
+  const email = document.getElementById("InputEmail").value;
+  const tel = document.getElementById("Inputtel").value;
+  const Password = document.getElementById("InputPassword").value;
+document.getElementById("Name").innerHTML="Name : "+firstName;
+document.getElementById("LastName").innerHTML= "last Name : "+lastname;
+document.getElementById("email").innerHTML= "Email : "+email;
+document.getElementById("tel").innerHTML= "telphone : "+tel;
+document.getElementById("password").innerHTML="Name : "+Password;
+}
+
+
+
